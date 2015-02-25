6d45745bf23e557e17197787fd75b278c3c0bd14
Wes Kaplan <me@weskaplan.com>
2015-02-24 18:40:46 -0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var path = require('path');
+
+var env = process.env.NODE_ENV || 'development';
+var port = process.env.PORT || 3000;
+
+var app = express();
+app.set('views', __dirname+ '/views');
+app.set('view engine', 'jade');
+
+app.use(express.static('public'))
+
+app.use(require('./routes'));
+
+app.listen(port);
+console.log('server is running!');
