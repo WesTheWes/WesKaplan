bb58941e53066dfc2b9dc80c3b999dbcc2f8945f
Wes Kaplan <me@weskaplan.com>
2015-03-15 22:51:51 -0700

    Reverting back

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var app = express();
 app.set('views', __dirname+ '/views');
 app.set('view engine', 'jade');
 
-app.use(express.static(__dirname + 'public'))
+app.use(express.static('public'))
 
 app.use(require('./routes'));
 
